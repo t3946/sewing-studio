384451398c2699932bc3ec9b24b16e074a6a9563
Vladislav <svialence@yandex.ru>
2022-10-02 00:36:01 +0300

    remove unused

--- server/src/app/controllers/app.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { AppService } from '@src/app/services/app.service';
-
-@Controller('api')
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-}
--- server/src/utils/config.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  passwordSalt: 't9g0of690fg89',
-};
